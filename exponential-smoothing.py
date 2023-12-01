dc5ade4f441a58ac9a21233f62e95dd2cd120446
SripaliManimekala <sripalimanimekala@gmail.com>
2023-12-01 21:25:52 +0530

    day1

--- exponential-smoothing.py
+++ exponential-smoothing.py
@@ -1,9 +1,9 @@
-N, alpha, K = map(float, input().split())
+N, alpha = map(float, input().split())
 data = [float(input()) for _ in range(int(N))]
 data_smoothed=[data[0]]
 
-for i in range(K):
-    smoothed_value = alpha * data[i] + (1 - alpha) * data_smoothed[i]
+for i in range(int(N)):
+    smoothed_value = alpha * data[i] + (1 - alpha) * data_smoothed[i-1]
     data_smoothed.append(smoothed_value)
 
 print(data)
